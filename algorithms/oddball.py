dade2ff56ef00abddd48998d16f6671ba420f022
tonnpa <tonnpa@gmail.com>
2015-11-24 12:12:33 +0100

    small fixes

--- algorithms/oddball.py
+++ algorithms/oddball.py
@@ -1,5 +1,3 @@
-from math import fabs, log, pow
-
 from matplotlib import pyplot as plt
 
 __author__ = 'tonnpa'
@@ -9,9 +7,8 @@ class Oddball:
     def __init__(self, graph):
         self._graph       = graph
         self._egonetworks = dict((node, {}) for node in graph.nodes())
-        self._outlierness = []
-        self._theta       = None
-        self._C           = None
+
+        self.run()
 
     @property
     def egonetworks(self):
@@ -21,10 +18,6 @@ class Oddball:
     def graph(self):
         return self._graph
 
-    @property
-    def outlierness(self):
-        return self._outlierness
-
     def run(self):
         for node in self.nodes():
             # extract egonetwork
@@ -32,49 +25,7 @@ class Oddball:
             self._egonetworks[node]['nodes'] = len(egonetwork.nodes())  # number of nodes in egonet
             self._egonetworks[node]['edges'] = len(egonetwork.edges())  # number of edges in egonet
 
-        nfeat = [self.n_count(n) for n in self.nodes()]
-        efeat = [self.e_count(n) for n in self.nodes()]
-        plt.suptitle('Node vs. Edge Feature')
-        plt.xlabel('#Nodes')
-        plt.ylabel('#Edges')
-        xmax = int(max(nfeat)*1.2)
-        ymax = int(max(efeat)*1.2)
-        plt.axis([0, xmax, 0, ymax])
-        # thresholds identifying extremes
-        plt.plot([i for i in range(1, xmax)], [i-1 for i in range(1,xmax)])
-        plt.plot([i for i in range(1, xmax)], [(i-1)*i/2 for i in range(1, xmax)])
-        plt.scatter(nfeat, efeat, c='c')
-
-        # for label, x, y in zip(labels, nfeat, efeat):
-        #     threshold = 0.75
-        #     if x > nmax*threshold or y > emax*threshold:
-        #         plt.annotate(
-        #             label,
-        #             xy=(x, y), xytext = (-15, 15),
-        #             textcoords = 'offset points',
-        #             horizontalalignment = 'right',
-        #             verticalalignment = 'bottom',
-        #             arrowprops = dict(arrowstyle='->', connectionstyle='arc3,rad=0')
-        #         )
-        #         txtfile.write(label + ' #nodes: ' + str(x) + ' #edges: ' + str(y) + ' quarter: ' + period + '\n')
-
-        plt.show()
-        plt.clf()
-        # # compute power-law coefficients m, C
-        # nodes = [log(self.n_count(n)    ) for n in self.nodes()]
-        # edges = [log(self.e_count(n) + 1) for n in self.nodes()]
-        # n_avg = float(np.mean(nodes))
-        # e_avg = float(np.mean(edges))
-        # n     = len(self.nodes())
-        #
-        # m = sum((nodes[i] * edges[i] for i in range(n))) - n * n_avg * e_avg
-        # m /= sum(n * n for n in nodes) - n * n_avg * n_avg
-        #
-        # logC = e_avg - m * n_avg
-        # C = exp(logC)
-        #
-        # self._theta, self._C = m, C
-        # self._outlierness = sorted([(node, self.out_line(node)) for node in self.nodes()], key=lambda t:t[1])
+        self.plot()
 
     def e_count(self, node):
         return int(self.egonetworks[node]['edges'])
@@ -85,7 +36,33 @@ class Oddball:
     def nodes(self):
         return self.graph.nodes()
 
-    def out_line(self, node):
-        y        = self.egonetworks[node]['edges']
-        Cx_theta = self._C * pow(self.n_count(node), self._theta)
-        return max(y, Cx_theta)/min(y, Cx_theta)*log(fabs(y-Cx_theta) + 1)
+    def plot(self, threshold=0.75):
+        labels, nfeat, efeat = [], [], []
+        for node in self.nodes():
+            labels.append(node)
+            nfeat.append(self.n_count(node))
+            efeat.append(self.e_count(node))
+
+        plt.title('Node vs. Edge Feature')
+        plt.xlabel('#Nodes')
+        plt.ylabel('#Edges')
+        x_max = int(max(nfeat)*1.2)
+        y_max = int(max(efeat)*1.2)
+        plt.axis([0, x_max, 0, y_max])
+        # thresholds identifying extremes
+        plt.plot([i for i in range(1, x_max)], [i-1 for i in range(1,x_max)])
+        plt.plot([i for i in range(1, x_max)], [(i-1)*i/2 for i in range(1, x_max)])
+        plt.scatter(nfeat, efeat, c='c')
+
+        for label, x, y in zip(labels, nfeat, efeat):
+            if x > x_max*threshold or y > y_max*threshold:
+                plt.annotate(
+                    label,
+                    xy=(x, y), xytext = (-15, 15),
+                    textcoords = 'offset points',
+                    horizontalalignment = 'right',
+                    verticalalignment = 'bottom',
+                    arrowprops = dict(arrowstyle='->', connectionstyle='arc3,rad=0')
+                )
+
+        plt.show()
--- coordinator.py
+++ coordinator.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot   as plt
 import networkx            as nx
 
 import algorithms.autopart as ap
-import algorithms.oddball  as ob
 import algorithms.scan     as sc
 
 __author__ = 'tonnpa'
@@ -29,36 +28,13 @@ example graphs adjacency matrix
  1.  0.  0.   1.  0.  0.  0.   0.  0.   1.  0.  1.  0.  0.
 """
 
+
 def main():
-    graphml = '.graphml'
-    graph   = nx.read_graphml(graphml, node_type=int)
-
-    oddball = ob.Oddball(graph)
-    oddball.run()
+    test_autopart()
+    test_scan()
 
 
-
-    # autopart = ap.Autopart(graphs)
-    # autopart.run()
-
-    # graphml  = '/home/tonnpa/Documents/datasets/example.graphml'
-    # graphs    = nx.read_graphml(graphml)
-
-    # g = nx.Graph()
-    # g.add_nodes_from([1,2,3,4])
-    # g.add_edges_from([(1,2),(3,4)])
-    # print nx.adj_matrix(g).todense()
-
-    # oddball  = ob.Oddball(graphs)
-    # oddball.run()
-    # print(oddball.egonetworks)
-    # print(oddball.outlierness)
-
-    # autopart = ap.Autopart(graphs)
-    # autopart.run()
-
-
-def run_example():
+def run_scan_example():
     graphml = '/home/tonnpa/Documents/datasets/example.graphml'
     graph = nx.read_graphml(graphml, node_type=int)
     scan_obj = sc.SCAN(graph)
@@ -86,7 +62,7 @@ def run_scan():
 
 
 def test_scan():
-    graphml = '/home/tonnpa/Documents/datasets/example.graphml'
+    graphml = 'graphs/examples/scan.graphml'
     graph = nx.read_graphml(graphml, node_type=int)
     scan_obj = sc.SCAN(graph)
     cores = set()
@@ -109,13 +85,13 @@ def test_scan():
 def test_autopart():
     from math import ceil, log
 
-    graphml  = '/home/tonnpa/Documents/datasets/example.graphml'
+    graphml  = 'graphs/examples/scan.graphml'
     graph    = nx.read_graphml(graphml)
     autopart = ap.Autopart(graph)
     autopart.k = 3
-    group_0 = [1,2,3]
-    group_1 = [4,5,6,7]
-    group_2 = [8,9]
+    group_0 = [1, 2, 3]
+    group_1 = [4, 5, 6, 7]
+    group_2 = [8, 9]
     autopart.map_g_n = {0: group_0, 1: group_1, 2: group_2}
 
     assert(autopart.group_size(0) == len(group_0))
@@ -127,9 +103,9 @@ def test_autopart():
     assert(ap.log2(20)  == log(20,  2))
 
     assert(autopart.code_group_sizes() == ceil(log(7, 2)) + ceil(log(4, 2)))
-    block_weights = ceil(log(16+1, 2)) + ceil(log(12+1, 2))*2 + \
-                    ceil(log( 9+1, 2)) + ceil(log( 8+1, 2))*2 + \
-                    ceil(log( 4+1, 2)) + ceil(log( 6+1, 2))*2
+    block_weights = ceil(log(16 + 1, 2)) + ceil(log(12 + 1, 2)) * 2 + \
+                    ceil(log( 9 + 1, 2)) + ceil(log( 8 + 1, 2)) * 2 + \
+                    ceil(log( 4 + 1, 2)) + ceil(log( 6 + 1, 2)) * 2
     assert (autopart.code_block_weights() == block_weights)
 
     assert(autopart.group_start_idx(0) == 0)
@@ -137,29 +113,28 @@ def test_autopart():
     assert(autopart.group_start_idx(2) == 7)
 
     # autopart._recalculate_block_properties()
-    assert(autopart.block_size(0,0) == 9)
-    assert(autopart.block_size(1,2) == 8)
-    assert(autopart.block_size(1,1) == 16)
+    assert(autopart.block_size(0, 0) == 9)
+    assert(autopart.block_size(1, 2) == 8)
+    assert(autopart.block_size(1, 1) == 16)
 
-    assert(autopart.block_weight(0,0) == 6)
-    assert(autopart.block_weight(2,1) == 3)
+    assert(autopart.block_weight(0, 0) == 6)
+    assert(autopart.block_weight(2, 1) == 3)
 
-    assert(autopart.block_density(0,0) == 6.0/9)
-    assert(autopart.block_density(2,1) == 3.0/8)
+    assert(autopart.block_density(0, 0) == (6.0 + 0.5) / (9 + 1))
+    assert(autopart.block_density(2, 1) == (3.0 + 0.5) / (8 + 1))
 
-    assert(autopart.row_weight(0,0) == 2)
-    assert(autopart.row_weight(1,1) == 1)
-    assert(autopart.row_weight(2,2) == 0)
-    assert(autopart.col_weight(0,2) == 0)
-    assert(autopart.col_weight(1,0) == 2)
-    assert(autopart.col_weight(2,1) == 1)
+    assert(autopart.row_weight(0, 0) == 2)
+    assert(autopart.row_weight(1, 1) == 1)
+    assert(autopart.row_weight(2, 2) == 0)
+    assert(autopart.col_weight(0, 2) == 0)
+    assert(autopart.col_weight(1, 0) == 2)
+    assert(autopart.col_weight(2, 1) == 1)
 
 
 def write_egonetworks():
-    from graphs import discussion_graph as dg
+    import graphs.portal444.discussion_graph as dg
 
     src_dir = '/tmp/posts'
-
     g = dg.build_graph(src_dir, g_path='/tmp/test.graphml')
     dg.write_egonets('/tmp/test.graphml', '/tmp/egonets')
 
